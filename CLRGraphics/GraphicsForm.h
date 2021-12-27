5cf78996e4497bd3828968e05c2e70d4aead4cfd
Aleksandr Rogalevich <sasa13481@gmail.com>
2021-12-27 13:50:07 +0300

    Добавили анимацию Рисования домика

--- CLRGraphics/GraphicsForm.h
+++ CLRGraphics/GraphicsForm.h
@@ -38,12 +38,13 @@ namespace CLRGraphics {
 	private: System::Windows::Forms::PictureBox^ picBox1;
 	protected:
 	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::NumericUpDown^ numericUpDown1;
 
 	private:
 		/// <summary>
 		/// ќб€зательна€ переменна€ конструктора.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+		System::ComponentModel::Container^ components;
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -54,14 +55,17 @@ namespace CLRGraphics {
 		{
 			this->picBox1 = (gcnew System::Windows::Forms::PictureBox());
 			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->numericUpDown1 = (gcnew System::Windows::Forms::NumericUpDown());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->picBox1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// picBox1
 			// 
+			this->picBox1->BackColor = System::Drawing::SystemColors::MenuHighlight;
 			this->picBox1->Location = System::Drawing::Point(121, 62);
 			this->picBox1->Name = L"picBox1";
-			this->picBox1->Size = System::Drawing::Size(716, 353);
+			this->picBox1->Size = System::Drawing::Size(1460, 746);
 			this->picBox1->TabIndex = 0;
 			this->picBox1->TabStop = false;
 			// 
@@ -75,32 +79,74 @@ namespace CLRGraphics {
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &GraphicsForm::button1_Click);
 			// 
+			// numericUpDown1
+			// 
+			this->numericUpDown1->Location = System::Drawing::Point(260, 13);
+			this->numericUpDown1->Name = L"numericUpDown1";
+			this->numericUpDown1->Size = System::Drawing::Size(120, 22);
+			this->numericUpDown1->TabIndex = 2;
+			// 
 			// GraphicsForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(1024, 495);
+			this->ClientSize = System::Drawing::Size(1593, 820);
+			this->Controls->Add(this->numericUpDown1);
 			this->Controls->Add(this->button1);
 			this->Controls->Add(this->picBox1);
 			this->Name = L"GraphicsForm";
 			this->Text = L"GraphicsForm";
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->picBox1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown1))->EndInit();
 			this->ResumeLayout(false);
 
 		}
 #pragma endregion
-	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
-		Graphics^ g = picBox1->CreateGraphics();
-		Pen^ pen = gcnew Pen(Color::Red);
-		g->DrawLine(pen,100, 0, 100, 100);
-		Thread::Sleep(500);
-		g->DrawLine(pen, 100, 100, 0, 100);
-		Thread::Sleep(500);
-		g->DrawLine(pen, 100, 100, 200, 100);
-		Thread::Sleep(500);
-		g->DrawLine(pen, 100, 100, 100, 500);
-		Thread::Sleep(500);
+		void drawdom(int mashtab) {
+			//picBox1->BackColor
+			//object for painting
+			Graphics^ g = picBox1->CreateGraphics();
+			//Clear Window
+			g->Clear(Color::DarkCyan);
+			//перо
+			Pen^ pen = gcnew Pen(Color::Red);
+			pen->Width = 3.0;
 
+			//Points for house
+			Point A(2 * mashtab, 6 * mashtab);
+			Point B(10 * mashtab, 1 * mashtab);
+			Point C(18 * mashtab, 6 * mashtab);
+			Point D(10 * mashtab, 2 * mashtab);
+			Point E(10 * mashtab, 5 * mashtab);
+			Point F(9 * mashtab, 3 * mashtab);
+			Point G(11 * mashtab, 3 * mashtab);
+			Point H(3 * mashtab, 17 * mashtab);
+			Point I(9 * mashtab, 17 * mashtab);
+			Point J(12 * mashtab, 9 * mashtab);
+			Point K(17 * mashtab, 17 * mashtab);
+			Point P(9 * mashtab, 8 * mashtab);
+
+			//Roof
+			g->DrawLine(pen, A, B);
+			g->DrawLine(pen, B, C);
+			g->DrawLine(pen, C, A);
+			//KREST!
+			g->DrawLine(pen, D, E);
+			g->DrawLine(pen, F, G);
+			//Walls
+			g->DrawLine(pen, A, H);
+			g->DrawLine(pen, H, K);
+			g->DrawLine(pen, K, C);
+			//Door
+			g->DrawRectangle(pen, 9 * mashtab, 8 * mashtab, 3 * mashtab, 9 * mashtab);
+		}
+
+
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		for (int i = 0; i < numericUpDown1->Value; i++) {
+			Thread::Sleep(100);
+			drawdom(i);
+		}
 	}
 	};
 }
